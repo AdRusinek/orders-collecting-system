eb531c50a268b29fb3db2a30b2040ab317ae25cf
AdRusinek <rusinekadrian19@gmail.com>
2020-07-24 13:17:35 +0200

    Adding sql script and config for caching as well as db connection. Closes #12

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (1, 'Pizza', 'Jakub Jakubowski', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (2, 'Woda', 'Aleksander Zakrzewski', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (3, 'Kwiatek', 'Urszula Kowalik', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (4, 'Szafa', 'Daria Janik', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (5, 'Kubek', 'Miłosz Chrzanowski', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (6, 'Plecak', 'Piotr Jabłoński', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (7, 'Myszka', 'Wiktor Krawczyk', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (8, 'Klawiatura', 'Urszula Andrzejewska', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (9, 'Laptop', 'Julia Janiszewska', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO ORDER_TABLE (id, order_name, customer_full_name, date_of_purchase, last_modified_date)
+values (10, 'Monitor', 'Natalia Szewczyk', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
