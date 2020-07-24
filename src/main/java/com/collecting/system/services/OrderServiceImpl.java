a8ef5eaef6952d2dfe4d95f6d028c9456b46a8d2
AdRusinek <rusinekadrian19@gmail.com>
2020-07-24 12:36:03 +0200

    Implementing service. Closes #7

--- src/main/java/com/collecting/system/services/OrderServiceImpl.java
+++ src/main/java/com/collecting/system/services/OrderServiceImpl.java
@@ -1,36 +1,73 @@
 package com.collecting.system.services;
 
+import com.collecting.system.domain.Order;
 import com.collecting.system.dto.OrderDto;
+import com.collecting.system.repositories.OrderRepository;
+import com.collecting.system.web.mappers.OrderMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Adrian Rusinek on 24.07.2020
  **/
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
 
     @Override
     public OrderDto placeOrder(OrderDto orderDto) {
-        return null;
+        log.info("New order '" + orderDto.getOrderName() +
+                "' have been placed for customer '" + orderDto.getCustomerFullName() + "'.");
+        return orderMapper.orderToOrderDto(orderRepository.save(orderMapper.orderDtoToOrder(orderDto)));
     }
 
     @Override
     public void deleteOrderById(Long id) {
+        findAndValidate(id);
 
+        log.info("Deleting order with id '" + id + "'.");
+        orderRepository.deleteById(id);
     }
 
     @Override
     public OrderDto getOrderById(Long id) {
-        return null;
+        log.info("Attempt of getting order with id '" + id + "'.");
+        return orderMapper.orderToOrderDto(findAndValidate(id));
     }
 
     @Override
     public List<OrderDto> listOrders(Boolean showRealTimeOrders) {
-        return null;
+        log.info("Getting all orders.");
+        return orderMapper.orderToOrdersDto(orderRepository.findAll());
     }
 
     @Override
-    public OrderDto updateOrder(Long id, OrderDto orderDto) {
-        return null;
+    public OrderDto updateOrder(Long id, OrderDto updatedOrder) {
+        Order foundOrder = findAndValidate(id);
+
+        foundOrder.setOrderName(updatedOrder.getOrderName());
+        foundOrder.setCustomerFullName(updatedOrder.getCustomerFullName());
+
+        log.info("Updating order with id '" + id + "'.");
+
+        return orderMapper.orderToOrderDto(orderRepository.save(foundOrder));
+    }
+
+    private Order findAndValidate(Long id) {
+        Optional<Order> optionalOrder = orderRepository.findById(id);
+
+        if (!optionalOrder.isPresent()) {
+            //todo throw exception
+        }
+
+        return optionalOrder.get();
     }
 }
