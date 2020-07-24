4d05a4d1e61a768bcb0d0fd8df85c6e76154ea73
AdRusinek <rusinekadrian19@gmail.com>
2020-07-24 12:48:39 +0200

    Adding controller methods. Closes #9

---
+++ src/main/java/com/collecting/system/web/OrderController.java
@@ -0,0 +1,74 @@
+package com.collecting.system.web;
+
+import com.collecting.system.dto.OrderDto;
+import com.collecting.system.services.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * Created by Adrian Rusinek on 22.07.2020
+ **/
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+    @Value("${secret-header}")
+    private String secretHeader;
+
+    @GetMapping
+    public ResponseEntity<?> listOrders() {
+
+        return new ResponseEntity<>(orderService.listOrders(null), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOrderById(@PathVariable Long id) {
+        return new ResponseEntity<>(orderService.getOrderById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> placeOrder(@Valid @RequestBody OrderDto orderDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            //todo validate errors
+        }
+
+        return new ResponseEntity<>(orderService.placeOrder(orderDto), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteOrderById(@PathVariable Long id, @RequestHeader(name = "secret") String secret) {
+
+        if (secret.equals(secretHeader)) {
+            orderService.deleteOrderById(id);
+            return new ResponseEntity<>("Order with id '" + id + "' was deleted.", HttpStatus.NO_CONTENT);
+        }
+
+        return respondNotAuthorized("delete");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateOrder(@Valid @RequestBody OrderDto orderDto, @PathVariable Long id, BindingResult bindingResult,
+                                         @RequestHeader(name = "secret") String secret) {
+        if (bindingResult.hasErrors()) {
+            //todo validate errors
+        }
+
+        if (secret.equals(secretHeader)) {
+            return new ResponseEntity<>(orderService.updateOrder(id, orderDto), HttpStatus.OK);
+        }
+
+        return respondNotAuthorized("update");
+    }
+
+    private ResponseEntity<?> respondNotAuthorized(String operation) {
+        return new ResponseEntity<>("You are not authorized to " + operation + " order.", HttpStatus.FORBIDDEN);
+    }
+}
